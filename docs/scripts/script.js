8562dfe62e7914eae322444705f39866928a553c
David Louis <eve.david.louis@gmail.com>
2018-07-26 18:28:53 +0200

    gitpages

---
+++ docs/scripts/script.js
@@ -0,0 +1,41 @@
+var data = [];
+var connection = '';
+
+function openSocket(){
+
+	// var host = 'wss://w-space.herokuapp.com/ws/test'
+	var host = 'wss://0.0.0.0/ws/test'
+
+	connection = new WebSocket(host);
+	
+	var body = $('body');
+
+	connection.onopen = function (event) {
+		body.html('<p>CONNECTION OPEN</p>');
+	};
+
+	connection.onclose = function (event) {
+		body.load('templates/offline.html');
+		setTimeout(openSocket, 10000);
+	};
+
+	connection.onerror = function (event) {
+		body.html('<p>ERROR</p>');
+	};
+
+	connection.onmessage = function (event) {
+		
+		data = JSON.parse(event.data);
+		update(data);
+	};
+}
+
+function update(updateData){
+
+	console.log('update')
+
+}
+
+$(document).ready(function(){
+	openSocket();
+});
