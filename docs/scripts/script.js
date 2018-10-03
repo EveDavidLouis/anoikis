453a174fbff0087e8b4ad8b6d77352ac1fc66d42
David Louis <eve.david.louis@gmail.com>
2018-10-03 13:13:43 +0200

    central

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -2,7 +2,7 @@ var connection;
 
 function openSocket(){
 
-	var host = 'wss://anoikis-eve-online.7e14.starter-us-west-2.openshiftapps.com'
+	var host = 'wss://space-anoikis.193b.starter-ca-central-1.openshiftapps.com'
 	if (window.location.hostname == '0.0.0.0' || window.location.hostname == 'localhost'){
 		host = 'ws://'+ window.location.host
 	};
