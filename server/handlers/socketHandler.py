ad36aaa0291638206311ff7ed225010e930a9b04
David Alimi <davidalimi@gmail.com>
2019-01-09 09:53:37 +0100

    Update socketHandler.py

--- server/handlers/socketHandler.py
+++ server/handlers/socketHandler.py
@@ -113,7 +113,7 @@ class SocketHandler(websocket.WebSocketHandler):
 
 		elif 'getCharacter' in inbound:
 
-			esi_api = yield db['pilots'].find_one({'esi_api.CharacterID':inbound['getCharacter'],'$or':[{'admin':{'$exists':1}},{'owner':self.CharacterID}]},{'_id':0,'esi_api.CharacterName':1,'esi_api.CharacterID':1,'corporationhistory':1,'stats':1,'standings':1,'wallet-journal':1,'wallet-transactions':1,'owner':1,'location':1,'bookmarks':1,'bookmarks-folders':1}) 	
+			esi_api = yield db['pilots'].find_one({'esi_api.CharacterID':inbound['getCharacter'],'$or':[{'group':{'$exists':1}},{'owner':self.CharacterID}]},{'_id':0,'esi_api.CharacterName':1,'esi_api.CharacterID':1,'corporationhistory':1,'stats':1,'standings':1,'wallet-journal':1,'wallet-transactions':1,'owner':1,'location':1,'bookmarks':1,'bookmarks-folders':1}) 	
 			
 			if esi_api:
 				outbound = {'endPoint':'character', 'data':esi_api}
@@ -139,7 +139,7 @@ class SocketHandler(websocket.WebSocketHandler):
 				query = {'owner':self.CharacterID}
 
 				if self.group == 'admin':
-					query = {}
+					query = {'esi_api':{'$exists':1}}
 
 			cursor = db['pilots'].find(query,{'_id':0,'esi_api.CharacterID':1,'esi_api.CharacterName':1})
 			charList = yield cursor.to_list(length=10000)
